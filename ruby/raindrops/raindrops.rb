a69a42d47bd9c5c40e12a656aba96d95f287cab5
Nathan L. Walls <nathan@wallscorp.us>
2015-09-24 22:40:39 -0400

    WIP: Non-functional

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -10,20 +10,29 @@ class Raindrops
       Factor.new(7, "Plong"),
     ]
 
-    responses = Array.new
-
-    factors.each do |factor|
-      if factor.factor_of?(number)
-        responses << factor.response
-      end
+    factors.inject(number) do |response, factor|
+      puts response
+      factor.calculate_response(number: number, response_context: response)
     end
-
-    responses.any? ? responses.join("") : number.to_s
   end
 end
 
 class Factor < Struct.new(:factor, :response)
   def factor_of?(number)
     number % factor == 0
+  end
+
+  def calculate_response(number:, response_context:)
+    case factor_of?(number)
+    when true && response_context.class == Fixnum
+      puts "hit true fixnum"
+      response
+    when true && response_context.class == String
+      puts "hit true string"
+      response_context + response
+    when false
+      puts "hit false"
+      response_context
+    end
   end
 end
