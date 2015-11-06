4eab2cf94a547b27b1141a1202574c0d5b6d5133
Nathan L. Walls <nathan@wallscorp.us>
2015-11-05 22:51:18 -0500

    Completes first ECMAScript exercise

---
+++ ecmascript/hello-world/hello-world.js
@@ -0,0 +1,16 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class HelloWorld {
+  hello(name = 'World') {
+    if (name.length === 0) {
+      name = 'World';
+    }
+
+    return ("Hello, " + name + '!');
+  }
+}
+
+export default HelloWorld;
