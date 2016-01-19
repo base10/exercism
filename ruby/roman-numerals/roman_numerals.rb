12959275ab884d00cba92c76394ed88097f44e90
Nathan L. Walls <nathan@wallscorp.us>
2016-01-18 22:06:21 -0500

    WIP: Setting up some rudimentary classes

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,33 +1,39 @@
 #!/usr/bin/env ruby
+require "pry"
 
 class Fixnum
   VERSION = 1
 
   def to_roman
-    if roman_numbers.member?(self)
-      roman_numbers.fetch(self).letter
-    else
-      convert
+    if Romannum.all_types_hash.member?(self)
+      Romannum.all_types_hash.fetch(self).letter
     end
   end
 
   private
 
-  def convert
-    puts "hi!"
-  end
-
-  def roman_numbers
-    {
-      1 => Romannum.new(letter: "I", number: 1),
-      5 => Romannum.new(letter: "V", number: 5),
-      10 => Romannum.new(letter: "X", number: 10)
-    }
-  end
 end
 
 class Romannum
   attr_reader :letter, :number
+
+  def self.all_types
+    @@all_types ||= [
+      new(letter: "I", number: 1),
+      new(letter: "V", number: 5),
+      new(letter: "X", number: 10),
+      new(letter: "L", number: 50),
+      new(letter: "C", number: 100),
+      new(letter: "D", number: 500),
+      new(letter: "M", number: 1000),
+    ]
+  end
+
+  def self.all_types_hash
+    @@all_types_hash = all_types.each_with_object({}) do |num, hash|
+      hash[num.number] = num
+    end
+  end
 
   def initialize(letter:, number:)
     @letter = letter
